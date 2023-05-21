061e87c318a7545325dad806432e86fdd9ab29b9
99xiaohao <3295459784@qq.com>
2023-05-22 00:59:26 +0800

    tijiao

--- app/src/main.js
+++ app/src/main.js
@@ -10,10 +10,13 @@ reqCategoryList()
 //第一个参数：全局组件的名字， 第二个参数：那个组件
 Vue.component(TypeNav.name,TypeNav)
 Vue.config.productionTip = false
-
+//引入仓库
+import store from '@/store'
 new Vue({
   render: h => h(App),
   //注册路由：底下的写法KV一致省略V{router小写的}
-  router
+  router,
+  //注册仓库：组件实例身上会多一个$store属性
+  store
 }).$mount('#app')
 
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -7,7 +7,9 @@ export default new Vuex.Store({
     //仓库存储数据的地方
     state:{},
     //修改state的唯一手段
-    mutations:{}
+    mutations:{},
     //处理action，可以书写自己的业务逻辑，也可以处理异步
-    
+    actions:{},
+    //getters:理解为计算属性，用于简化仓库数据，让组件获取仓库的数据更加方便
+    getters:{},
 })
